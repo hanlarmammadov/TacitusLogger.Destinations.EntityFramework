5732504628b6485ec1bb972395495f9e85c7913e
Khanlar Mammadov <khanlar@mammadov.pro>
2020-03-01 04:46:59 +0400

    Examples project added with config examples

---
+++ examples/TacitusLogger.Destinations.EntityFramework.Examples/MyCustomDbEntity.cs
@@ -0,0 +1,13 @@
+﻿using System; 
+
+namespace TacitusLogger.Destinations.EntityFramework.Examples
+{
+    class MyCustomDbEntity
+    {
+        public string LogId { get; set; }
+        public string Context { get; set; }
+        public LogType LogType { get; set; }
+        public string Description { get; set; }
+        public DateTime LogDate { get; set; }
+    }
+}
